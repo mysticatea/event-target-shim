fedf0da638cf614610b5c9c0fcf01972c20518ae
Toru Nagashima <star.ctor@gmail.com>
2015-04-19 08:56:09 +0900

    Update dependencies.

--- karma.conf.js
+++ karma.conf.js
@@ -2,18 +2,19 @@ module.exports = function(config) {
   config.set({
     basePath: "",
     frameworks: ["browserify", "mocha"],
-    files: [
-      "test/*.js"
-    ],
+    files: ["test/*.js"],
     browsers: ["Chrome", "Firefox", "IE"],
 
     preprocessors: {
       "test/*.js": ["browserify"]
     },
+
     browserify: {
       debug: true,
       extensions: [".js"],
-      transform: ["babelify", "espowerify"]
+      transform: [
+        ["babelify", {"plugins": ["babel-plugin-espower"]}]
+      ]
     }
   });
 };
