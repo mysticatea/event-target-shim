6583d2648577b7f7c7e11ad02a4e0bf69433b8f3
Xiaomi-Yule <fishle2016@qq.com>
2019-11-20 17:07:10 +0800

    change console.assert to throw new TypeError
    on react-native 0.61, use console.assert may get
    "console.assert is not a function" error

--- dist/event-target-shim.js
+++ dist/event-target-shim.js
@@ -43,11 +43,9 @@ const wrappers = new WeakMap();
  */
 function pd(event) {
     const retv = privateData.get(event);
-    console.assert(
-        retv != null,
-        "'this' is expected an Event object, but got",
-        event
-    );
+    if(retv === null){
+        throw new TypeError("'this' is expected an Event object, but got " + JSON.stringify(event));
+    }
     return retv
 }
 
